81a44aaa0356d6ee4ce9bafd9d672eee70f1885a
Piyush Kumar <code502997@gmail.com>
2026-06-22 17:32:27 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,29 +1,4 @@
-/*const express = require("express");
-const cors = require("cors");
 
-const connectDB = require("./config/db");
-
-const userRoutes = require("./routes/userRoutes");
-const resultRoutes = require("./routes/resultRoutes");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-connectDB();
-
-app.use("/api", userRoutes);
-
-app.use("/api/results", resultRoutes);
-
-app.listen(5000, () => {
-
-    console.log(
-        "Server running on port 5000"
-    );
-
-});*/
 
 const express = require("express");
 const cors = require("cors");
