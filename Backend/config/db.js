f38ba0e8f91d37bb24792d420cd5917d680fd822
Piyush Kumar <212510220+Piyush29k@users.noreply.github.com>
2026-06-26 13:17:19 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    const uri =
+      process.env.MONGODB_URI ||
+      "mongodb+srv://piyushk350m_db_user:dVWNXAXrT5wOysy9@cluster0.ajvzdtb.mongodb.net/myapp?retryWrites=true&w=majority";
 
-    await mongoose.connect(
-      "mongodb+srv://piyushk350m_db_user:dVWNXAXrT5wOysy9@cluster0.ajvzdtb.mongodb.net/myapp?retryWrites=true&w=majority"
-    );
+    await mongoose.connect(uri);
 
     console.log("MongoDB Connected ✅");
 
